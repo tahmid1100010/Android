29d6f621973079a27bfbf755e00357482cb65170
tahmid1100010 <tjawad98@gmail.com>
2020-08-16 18:00:04 +0600

    slight changes

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.firstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSecond();
+            }
+        });
+    }
+
+    public void openSecond(){
+        Intent intent = new Intent(this,Second.class);
+        startActivity(intent);
     }
 }
